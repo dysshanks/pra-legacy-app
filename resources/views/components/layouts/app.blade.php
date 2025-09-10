c461a268eabf4c55e51ae698ece1f6eff0bead65
dysshanks <ryanvdvorst@outlook.com>
2025-09-10 10:01:06 +0200

    moved the footer to the bottom

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -34,9 +34,6 @@
 
         </div>
 
-        <div class="row">
-            <x-footer/>
-        </div>
 
     </div>
 
@@ -49,6 +46,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script>//window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
 <script src="{{ asset('/js/app.js') }}"></script>
-
+<x-footer/>
 </body>
 </html>
