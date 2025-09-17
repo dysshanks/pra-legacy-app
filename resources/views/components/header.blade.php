a709ebd490b3355fc45bdfe831037d8c80185874
dysshanks <ryanvdvorst@outlook.com>
2025-09-17 10:07:29 +0200

    ticket 7

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
 <div class="jumbotron">
     <div class="container">
         <a href="/" title="{{ __('misc.home_alt') }}" alt="{{ __('misc.home_alt') }}">
-            <h1>{{ __('misc.homepage_title') }}</h1>
+            <h1 class="bg-blue button d-inline text-white">{{ __('misc.homepage_title') }}</h1>
         </a>
         {{ $introduction_text ?? '' }}
     </div>
